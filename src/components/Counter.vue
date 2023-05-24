42df5102ebd4cab0353c501b9940689742323aad
Karolina Redden <dubajkaro@gmail.com>
2023-05-23 22:09:36 -0300

    use script setup to pass props allowing for less code in props defining

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -3,11 +3,13 @@
     <p>Count Value: {{ count }}</p>
   </div>
 </template>
-<script>
-export default {
-  props: ["count"],
-  setup(props) {
-    console.log("props count", props.count);
-  },
-};
+<script setup>
+// export default {
+//   props: ["count"],
+//   setup(props) {
+//     console.log("props count", props.count);
+//   },
+// };
+const props = defineProps(["count"]);
+const count = props.count;
 </script>
