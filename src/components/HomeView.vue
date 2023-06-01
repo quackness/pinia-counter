9f751488045b61205ba40b637d62db6a974fc0fb
Karolina Redden <dubajkaro@gmail.com>
2023-06-01 16:12:02 -0300

    use storeToRefs

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup>
 import Counter from "@/components/Counter.vue";
 import { useAuthStore } from "@/stores/auth";
+import { storeToRefs } from "pinia";
 const authStore = useAuthStore();
+const { isAuthenticated, user } = storeToRefs(authStore);
 </script>
 
 <template>
   <main>
-    <div v-if="authStore.isAuthenticated">
-      User name is: {{ authStore.user.name }}
-    </div>
+    <div v-if="isAuthenticated">User name is: {{ user.name }}</div>
     <Counter />
   </main>
 </template>
